0d80957bcd2bc4459d687b2a2101951368c2fd9a
jcccn <jccuestc@gmail.com>
2013-09-01 21:32:46 +0800

    add the GHUnit test framework

---
+++ Gingko-iOS/GingkoTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GingkoTests
+//
+//  Created by Jiang Chuncheng on 9/1/13.
+//  Copyright (c) 2013 senseforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <GHUnitIOS/GHUnitIOSAppDelegate.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHUnitIOSAppDelegate class]));
+    }
+}
